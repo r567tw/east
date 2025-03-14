ec5a2b2d8e55d05724528548078b987173690024
r567tw <r567tw@gmail.com>
2025-03-14 13:39:35 +0800

    task css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Tasks</title>
 </head>
 
-<body>
+<body class="container">
     <h1>Tasks</h1>
-    <ul>
+    <ul class="list-unstyled">
         @foreach ($tasks as $task)
         <li>
-            <label><input type="checkbox" name="completed" id="">{{ $task->title }}</label>
+            <label>
+                <input type="checkbox" name="completed" id="" class="form-group mr-2">
+                {{ $task->title }}
+            </label>
         </li>
         @endforeach
     </ul>
