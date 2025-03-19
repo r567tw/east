7d6f12f41e7f6602dd831efbbbcd09d2a9fb229a
r567tw <r567tw@gmail.com>
2025-03-19 23:58:31 +0800

    Change Folder Structure

--- Dockerfile
+++ Dockerfile
@@ -27,7 +27,7 @@ RUN groupadd -g 1000 www
 RUN useradd -u 1000 -ms /bin/bash -g www www
 
 # Copy existing application directory contents
-COPY . /var/www
+COPY ./src /var/www
 
 # Copy existing application directory permissions
 COPY --chown=www:www . /var/www
--- src/routes/web.php
+++ src/routes/web.php
@@ -6,14 +6,22 @@ use App\Models\Task;
 
 Route::get("/", [HomeController::class, 'home']);
 
-
-
 Route::get('/tasks', function () {
-    $tasks = Task::all();
+    $tasks = Task::latest()->get();
     return view('index', ['tasks' => $tasks]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
-    $task = Task::findOrfail($id);
+    $task = Task::findOrFail($id);
     return view('show', ['task' => $task]);
 })->name('tasks.show');
+
+Route::post('/tasks', function () {
+    $task = new Task();
+    $task->title = request('title');
+    $task->description = request('description');
+    $task->save();
+    return redirect()->route('tasks.index');
+})->name('tasks.store');
