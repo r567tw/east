feed91291fb1fbeab2539a275131636a007f8548
r567tw <r567tw@gmail.com>
2025-03-30 18:40:59 +0800

    feat: add authentication functionality with login endpoint and user model integration

---
+++ src/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !password_verify($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json(['token' => $user->createToken('api_token')->plainTextToken]);
+    }
+
+    public function logout(Request $request) {}
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\HomeController;
-use App\Models\Attendee;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -12,5 +12,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('up', [HomeController::class, 'index']);
+Route::post('login', [AuthController::class, 'login']);
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class);
