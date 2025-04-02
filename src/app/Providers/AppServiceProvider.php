f1ef7a37f8e193c991ed4fa6cb9c30896ac6a4b4
r567tw <r567tw@gmail.com>
2025-04-02 22:30:09 +0800

    Queue and RateLimiter

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -31,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
             return Limit::perHour(3)->by($request->user()?->id ?: $request->ip());
         });
 
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
         // Gate::define('update-event', function (User $user, Event $event) {
         //     return $user->id === $event->user_id;
         // });
--- src/routes/api.php
+++ src/routes/api.php
@@ -16,6 +16,6 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class)->only(['index', 'show']);
-Route::apiResource('events', EventController::class)->only(['store', 'update', 'destroy'])->middleware('auth:sanctum');
+Route::apiResource('events', EventController::class)->only(['store', 'update', 'destroy'])->middleware(['auth:sanctum', 'throttle:api']);
 Route::apiResource('events.attendees', AttendeeController::class)->only(['index', 'show']);
-Route::apiResource('events.attendees', AttendeeController::class)->only(['store', 'update', 'destroy'])->middleware('auth:sanctum');
+Route::apiResource('events.attendees', AttendeeController::class)->only(['store', 'update', 'destroy'])->middleware(['auth:sanctum', 'throttle:api']);
