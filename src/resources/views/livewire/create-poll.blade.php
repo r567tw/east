71bdb5a38a089e96cce7bc8173d83a26b533e12a
r567tw <r567tw@gmail.com>
2025-04-05 11:39:55 +0800

    Add Livewire component for creating polls with a corresponding view

---
+++ src/resources/views/livewire/create-poll.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h1>Create Poll</h1>
+    <form action="">
+        <label for="">Poll Title</label>
+        <input type="text" wire:model.live="title">
+        {{ $title }}
+    </form>
+
+</div>
