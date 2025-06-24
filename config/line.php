21536865ccf5ee8c245b16f88bfbdb2c122c09f5
r567tw <r567tw@gmail.com>
2025-06-24 23:15:40 +0800

    update config

--- config/line.php
+++ config/line.php
@@ -3,6 +3,6 @@
 return [
 
     'line_channel_secret' => env('LINE_CHANNEL_SECRET'),
-    'line_access_token' => env('LINE_ACCESS_TOKEN'),
+    'line_access_token' => env('LINE_CHANNEL_ACCESS_TOKEN'),
 
 ];
