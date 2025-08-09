b67240e8004f8f9b0e17e898f97ad9adb600ebfe
r567tw <r567tw@gmail.com>
2025-08-09 14:20:07 +0800

    優化

--- app/Helpers/WeatherHelper.php
+++ app/Helpers/WeatherHelper.php
@@ -9,16 +9,29 @@ class WeatherHelper
     public function getWeather($locationName = '高雄市'): string
     {
         $weatherApiKey = config('app.cwb_api_key');
-        $url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={$weatherApiKey}&format=JSON&locationName={$locationName}";
+        $url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={$weatherApiKey}&format=JSON&locationName={$locationName}&elementName=Wx,PoP,CI";
         $response = Http::get($url);
 
         if ($response->successful()) {
             $data = $response->json();
-            $weatherInfo = $data['records']['location'][0]['weatherElement'][0]['time'][0]['parameter']['parameterName'];
-            $rainyProbability = $data['records']['location'][0]['weatherElement'][1]['time'][0]['parameter']['parameterName'];
-            return "天氣查詢結果：{$weatherInfo}\n降雨機率：{$rainyProbability}%";
+
+            $weatherInfo = $this->getWeatherElement('Wx', $data);
+            $rainyProbability = $this->getWeatherElement('PoP', $data);
+            $comfortIndex = $this->getWeatherElement('CI', $data);
+
+            return "天氣查詢結果：\n{$weatherInfo} 舒適度:{$comfortIndex}\n降雨機率：{$rainyProbability}%";
         } else {
             return "天氣查詢失敗，請稍後再試。";
         }
+    }
+
+    private function getWeatherElement($name, $data): string
+    {
+        foreach ($data['records']['location'][0]['weatherElement'] as $element) {
+            if ($element['elementName'] === $name) {
+                return $element['time'][0]['parameter']['parameterName'];
+            }
+        }
+        return '';
     }
 }
