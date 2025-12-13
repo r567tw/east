bc2273ee7bb95d1016ecaa54f2041c0f4b6a7fe3
r567tw <r567tw@gmail.com>
2025-12-14 00:40:44 +0800

    test pre-commit

--- app/Models/ShortUrl.php
+++ app/Models/ShortUrl.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ShortUrl extends Model
 {
