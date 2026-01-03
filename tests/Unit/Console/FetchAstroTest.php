74b8941add3e157025e607c8640b6658414e8646
r567tw <r567tw@gmail.com>
2026-01-03 14:30:21 +0800

    add HTTP fake responses for console tests

--- tests/Unit/Console/FetchAstroTest.php
+++ tests/Unit/Console/FetchAstroTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Unit\Console;
 
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class FetchAstroTest extends TestCase
 {
     public function test_command_runs()
     {
+        Http::fake([
+            '*' => Http::response(file_get_contents(__DIR__ . '/../../Stubs/astro_response.html'), 200),
+        ]);
         $this->artisan('app:fetch-astro')
             ->assertExitCode(0);
     }
--- tests/Unit/Console/GetGoldPriceTest.php
+++ tests/Unit/Console/GetGoldPriceTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Unit\Console;
 
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class GetGoldPriceTest extends TestCase
 {
     public function test_command_runs()
     {
+        Http::fake([
+            'https://www.gold.org.tw/*' => Http::response(file_get_contents(__DIR__ . '/../../Stubs/gold_price_response.html'), 200),
+        ]);
         $this->artisan('app:get-gold-price')
             ->assertExitCode(0);
     }
