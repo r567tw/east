cf6df26896857e7f60b594bd4378ee5d517bdc97
r567tw <r567tw@gmail.com>
2026-01-04 19:19:11 +0800

    remove CleanInviteCode command and related tests

--- routes/console.php
+++ routes/console.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Schedule;
 // })->purpose('Display an inspiring quote')->everyMinute()->sendOutputTo(storage_path('logs/inspire.log'));
 
 Schedule::command('app:get-gold-price')->hourly();
-Schedule::command('app:clean-invite-code')->daily();
+// Schedule::command('app:clean-invite-code')->daily();
 // Schedule::command('app:clean-short-url')->daily();
 Schedule::command('app:fetch-astro')->daily();
 Schedule::command('app:routine-reminder')->weeklyOn(0, '00:30'); // 每週日凌晨12:30
