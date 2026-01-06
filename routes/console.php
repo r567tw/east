f50f0d510fe4afd5fd925912807841ed5cc63ba9
r567tw <r567tw@gmail.com>
2026-01-06 22:35:02 +0800

    remove unused commands from console schedule

--- routes/console.php
+++ routes/console.php
@@ -1,15 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-// Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote());
-// })->purpose('Display an inspiring quote')->everyMinute()->sendOutputTo(storage_path('logs/inspire.log'));
-
 Schedule::command('app:get-gold-price')->hourly();
-// Schedule::command('app:clean-invite-code')->daily();
-// Schedule::command('app:clean-short-url')->daily();
 Schedule::command('app:fetch-astro')->daily();
 Schedule::command('app:routine-reminder')->weeklyOn(0, '00:30'); // 每週日凌晨12:30
